332b9dbe3a5d8f168c5c05e1fa3e77896a0a1aa2
glebgrachev <priorcom24@gmail.com>
2026-08-05 16:35:28 +0300

    fix: add GET method to CORS allowed methods

--- lib/cors.ts
+++ lib/cors.ts
@@ -1,6 +1,6 @@
 const cors = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
   "Access-Control-Allow-Headers": "content-type",
 };
 
